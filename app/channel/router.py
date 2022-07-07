356281683e44adc8995f9eb8a206f6bd9f06a4c9
Jonghyeon Kim <85392646+ryankimjh00@users.noreply.github.com>
2022-07-07 14:59:16 +0900

    request modified_1

--- app/channel/router.py
+++ app/channel/router.py
@@ -2,42 +2,50 @@
 from pydantic import BaseModel
 from fastapi import APIRouter, File, HTTPException
 from datetime import datetime
-# from typing import Union
+
 router = APIRouter(prefix='/channel', tags=['channel'])
 
-"""Channels"""
 
-class MyChannel(BaseModel):
-    name: str | None = None
-    id: int
-    info: str | None = None
+# Channels
 
 @router.get('/create_channel')
-async def get_channel_info(channel_id: int, channel_name: str | None = "unnamed"):
+async def get_channel_info(channel_id: int, channel_name: str, channel_date: datetime):
     return {"channel_id": channel_id,
-            "channel_name": channel_name}
+            "channel_name": channel_name,
+            "channel_date": channel_date}
 
 
 @router.get('/create_channel/{channel_member}')
-async  def get_channel_info(member_name: str):
+async def get_channel_info(member_name: str):
     return {member_name}
 
-@router.get('/create_channel/get_channel_info/{get_channel_feature}')
+
+@router.get('/create_channel/channel_info/{channel_feature}')
 async def get_channel_feature(feature: None):
     return feature
+
 
 @router.get('/create_channel/channel_info/{channel_date}')
 async def get_channel_datetime(date: datetime):
     return date
 
-@router.delete('/create_channel/{get_channel_info}')
+
+@router.put('/create_channel/{channel_info}')
+async def update_channel(new_channel_name: str, update_date: datetime):
+    get_channel_info.channel_name = new_channel_name
+    get_channel_info.channel_date = update_date
+    return {get_channel_info}
+
+
+@router.delete('/create_channel/{channel_info}')
 async def delete_channel_info(member_name: str):
     member_name = get_channel_info.get(member_name)
     if member_name not in get_channel_info():
-        raise HTTPException(status_code=404, datail = "404 not found")
+        raise HTTPException(status_code=404)
     get_channel_info.delete(member_name)
     get_channel_info.commit()
-    return{"deleted": True}
+    return {"deleted": True}
+
 
 # chats
 
@@ -46,13 +54,23 @@ class Chat(BaseModel):
     content: str
     date: datetime
 
-@router.post('/create_chat', response_model=Chat)
+
+@router.get('/create_chat', response_model=Chat)
 async def create_chat(chat: Chat):
     return chat
 
+
 @router.get('/create_chat/{show_chat}')
 async def show_chat(chat: Chat):
-    return{"content": Chat.content,
-           "chatter": Chat.chatter,
-           "date": Chat.date}
+    return {"content": chat.content,
+            "chatter": chat.chatter,
+            "date": chat.date}
 
+
+@router.delete('/create_chat')
+async def delete_chat(chat: Chat):
+    if chat not in chat:
+        raise HTTPException(status_code=404, detail="404 chat not found")
+    create_chat.delete(Chat)
+    create_chat.commit()
+    return {"chat deleted": True}
